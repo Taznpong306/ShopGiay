36f9527ca9dcb4bbe44a82cdac8877969ba99891
Taznpong306 <phongkudo85@gmail.com>
2024-06-26 16:43:03 +0700

    Add project files.

---
+++ WebBanHang/Areas/Customer/Controllers/ProductdetailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace PhoneStore.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductdetailController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductdetailController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Productdetail(string id)
+        {
+            var product = _db.Products.FirstOrDefault(x => x.Id == int.Parse(id));
+            return View(product);
+        }
+    }
+}
---
+++ WebBanHang/Areas/Customer/Controllers/ProductsearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+
+namespace PhoneStore.Areas.Customer.Controllers
+    {[Area("Customer")]
+    public class ProductsearchController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductsearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult ProductSearch(string searchTerm)
+        {
+            var product = _db.Products.Where(x => x.Name.Contains(searchTerm)).ToList();
+            ViewBag.SearchTerm = searchTerm;
+            return View(product);
+        }
+
+    }
+}
